9a4316cef9ebb978e2b9670fbe2fa653688645bc
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-08 12:17:17 +0800

    feature(chapter01): Listing 1-22. Manual type variations

---
+++ chapter01/src/22.ts
@@ -0,0 +1,24 @@
+// Listing 1-22. Manual type variations
+
+interface Options {
+  material: string
+  backlight: boolean
+}
+
+// Manually created readonly interface
+interface ManualReadonlyOptions {
+  readonly material: string
+  readonly backlight: boolean
+}
+
+// Manually created optional interface
+interface ManualOptionalOptions {
+  material?: string
+  backlight?: string
+}
+
+// Manually created nullable interface
+interface ManualNullableOptions {
+  material: string | null
+  backlight: string | null
+}

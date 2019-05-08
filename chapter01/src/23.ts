63c5512050881c307d32f0ce5cca13e09e425ffe
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-08 12:18:26 +0800

    feature(chapter01): Listing 1-23. Mapped types

---
+++ chapter01/src/23.ts
@@ -0,0 +1,15 @@
+// Listing 1-23. Mapped types
+interface Options {
+  material: string
+  backlight: boolean
+}
+
+// Mapped types
+type ReadOnly<T> = { readonly [k in keyof T]: T[k] }
+type Optional<T> = { [k in keyof T]?: T[k] }
+type Nullable<T> = { [k in keyof T]: T[k] | null }
+
+// Creating new types from mapped types
+type ReadonlyOptions = Readonly<Options>
+type OptionalOptions = Optional<Options>
+type NullableOptions = Nullable<Options>

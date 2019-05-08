48fb3d9d52350573d988233d388b5af1046ff1bc
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-08 11:56:55 +0800

    feature(chapter01): Listing 1-16. String literal type

---
+++ chapter01/src/16.ts
@@ -0,0 +1,16 @@
+// Listing 1-16. String literal type
+type Kingdom =
+  | "Bacteria"
+  | "Protozoa"
+  | "Chromista"
+  | "Plantae"
+  | "Fungi"
+  | "Animalia"
+
+let kingdom: Kingdom
+
+// OK
+kingdom = "Bacteria"
+
+// Error: Type 'Protista' is not assignable to type 'Kingdom'
+//kingdom = "Protista"

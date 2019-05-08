0a87a3670e7a23efc0d07ae641ae76bc5962f3d1
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-08 09:23:55 +0800

    feature(chapter01): Listing 1-12. Flags

---
+++ chapter01/src/12.ts
@@ -0,0 +1,11 @@
+// Listing 1-12. Flags
+
+enum DiscFlags {
+  None = 0,
+  Drive = 1,
+  Influence = 2,
+  Steadiness = 4,
+  Conscientiousness = 8,
+}
+
+export { DiscFlags }
---
+++ chapter01/test/12.ts
@@ -0,0 +1,28 @@
+import { DiscFlags } from "../src/12"
+
+import assert from "assert"
+
+describe("Listing 1-12. Flags", function() {
+  it("should checking the personality flags correctly", function() {
+    // Using flags
+    const personality = DiscFlags.Drive | DiscFlags.Conscientiousness
+
+    // Testing flags
+
+    const hasD = (personality & DiscFlags.Drive) == DiscFlags.Drive
+    assert.ok(hasD)
+
+    // false
+    const hasI = (personality & DiscFlags.Influence) == DiscFlags.Influence
+    assert.ok(!hasI)
+
+    // false
+    const hasS = (personality & DiscFlags.Steadiness) == DiscFlags.Steadiness
+    assert.ok(!hasS)
+
+    // true
+    const hasC =
+      (personality & DiscFlags.Conscientiousness) == DiscFlags.Conscientiousness
+    assert.ok(hasC)
+  })
+})

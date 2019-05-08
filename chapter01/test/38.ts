fa008a6a6827a351af0bf92ec508042e0bd1fef3
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-08 13:03:35 +0800

    feature(chapter01): Listing 1-38. Short-circuit evaluation

---
+++ chapter01/test/38.ts
@@ -0,0 +1,17 @@
+//import assert from "assert"
+
+describe("Listing 1-38. Short-circuit evaluation", () => {
+  it("", () => {
+    /*
+    interface Caravan {
+      rooms: number
+    }
+
+    let caravan: Caravan
+    if (caravan && caravan.rooms > 5) {
+      //...
+      assert.fail()
+    }
+    */
+  })
+})

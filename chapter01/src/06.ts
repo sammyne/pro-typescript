783e03ac63d7f28e4ea89d01e86b7ed2bc3b8a5b
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-08 08:21:19 +0800

    feature(chapter01): Listing 1-6. Constants

---
+++ chapter01/src/06.ts
@@ -0,0 +1,13 @@
+// Listing 1-6. Constants
+
+;(() => {
+  const name = "Lily"
+
+  // Error! Cannot assign to name because it is a constant
+  //name = "Princess Sparkles"
+
+  const digits = [1, 2, 3]
+
+  // Mutable - this changes the value of digits without using an assignment
+  digits.push(4, 5, 6)
+})()

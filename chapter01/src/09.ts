c219f8ab866e90173b1648a327bfdbd2688adbca
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-08 08:26:43 +0800

    feature(chapter01): Listing 1-9. Interface and type alias

---
+++ chapter01/src/09.ts
@@ -0,0 +1,23 @@
+// Listing 1-9. Interface and type alias
+
+// Interface
+interface PersonInterface {
+  name: string
+  heightInCentimeters: number
+}
+
+const sherlock: PersonInterface = {
+  name: "Bendict",
+  heightInCentimeters: 183,
+}
+
+// Type Alias
+type PersonType = {
+  name: string
+  heightInCentimeters: number
+}
+
+const john: PersonType = {
+  name: "Martin",
+  heightInCentimeters: 169,
+}

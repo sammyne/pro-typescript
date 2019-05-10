22f67c57ec8469e9f4c7662cbed278906e42890e
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-10 12:54:03 +0800

    feature(chapter01): Listing 1-69. Extending the NodeList interface

---
+++ chapter01/src/69.ts
@@ -0,0 +1,11 @@
+// Listing 1-69. Extending the NodeList interface
+
+interface NodeList {
+  onclick: (event: MouseEvent) => any
+}
+
+const nodeList = document.getElementsByTagName("div")
+
+nodeList.onclick = function(event: MouseEvent) {
+  alert("Clicked")
+}

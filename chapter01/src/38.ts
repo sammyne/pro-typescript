fde931ad001080d824d6b46862dfdc65c982daa9
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-10 19:53:16 +0800

    refactor(chapter01): move listing-38 back to src

---
+++ chapter01/src/38.ts
@@ -0,0 +1,16 @@
+// Listing 1-38. Short-circuit evaluation
+
+namespace Listing38 {
+  /*
+  interface Caravan {
+    rooms: number
+  }
+
+  let caravan: Caravan
+
+  if (caravan && caravan.rooms > 5) {
+    //...
+    assert.fail()
+  }
+  */
+}

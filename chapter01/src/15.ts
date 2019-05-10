d99b021dd34b20d703b7ee3ff020dc699be65cb8
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-10 18:55:15 +0800

    refactor(chapter01): scope listing15 with namespace

--- chapter01/src/15.ts
+++ chapter01/src/15.ts
@@ -1,19 +1,22 @@
 // Listing 1-15. Union Types
 
-// Type annotation for a union type
-let union: boolean | number
+namespace Listing15 {
 
-// OK: number
-union = 5
+  // Type annotation for a union type
+  let union: boolean | number
 
-// OK: boolean
-union = true
+  // OK: number
+  union = 5
 
-// Error: Type "string" is not assignable to type 'number | boolean'
-//union = "string"
+  // OK: boolean
+  union = true
 
-// Type alias for a union type
-type StringOrError = string | Error
+  // Error: Type "string" is not assignable to type 'number | boolean'
+  //union = "string"
 
-// Type alias for union of many types
-type SeriesOfTypes = string | number | boolean | Error
+  // Type alias for a union type
+  type StringOrError = string | Error
+
+  // Type alias for union of many types
+  type SeriesOfTypes = string | number | boolean | Error
+}

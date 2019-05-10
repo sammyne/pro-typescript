4989c730fc379e5b394993b86ab3e6d32b36ba6d
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-10 08:05:02 +0800

    feature(chapter10): Listing 10-5. Extending the specification

--- chapter09/test/FizzBuzz.test.ts
+++ chapter09/test/FizzBuzz.test.ts
@@ -9,4 +9,11 @@ describe("A FizzBuzz generator", () => {
 
     expect(result).to.equal(1)
   })
+
+  it("should return the number 2 when 2 is played", () => {
+    const fizzBuzz = new FizzBuzz()
+    const result = fizzBuzz.generate(2)
+
+    expect(result).to.equal(2)
+  })
 })

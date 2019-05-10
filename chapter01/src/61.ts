c094e017afbd6159f239ace74687640bcd7a70e9
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-10 12:20:46 +0800

    feature(chapter01): Listing 1-61. Arrow functions

---
+++ chapter01/src/61.ts
@@ -0,0 +1,7 @@
+const shortAddNumbers = (a: number, b: number) => a + b
+const mediumAddNumbers = (a: number, b: number) => {
+  return a + b
+}
+const longAddNumbers = function(a: number, b: number) {
+  return a + b
+}

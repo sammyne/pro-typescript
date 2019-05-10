dbd21b88d270a56475ca0408d2c4000b1719cad1
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-10 08:08:35 +0800

    feature(chapter10): Listing 10-9. Refactored specifications

--- chapter09/test/FizzBuzz.test.ts
+++ chapter09/test/FizzBuzz.test.ts
@@ -3,22 +3,25 @@ import { FizzBuzz } from "../src/FizzBuzz"
 import { expect } from "chai"
 
 describe("A FizzBuzz generator", () => {
+  let fizzBuzz: FizzBuzz
+
+  beforeEach(() => {
+    fizzBuzz = new FizzBuzz()
+  })
+
   it("should return the number 1 when 1 is played", () => {
-    const fizzBuzz = new FizzBuzz()
     const result = fizzBuzz.generate(1)
 
     expect(result).to.equal(1)
   })
 
   it("should return the number 2 when 2 is played", () => {
-    const fizzBuzz = new FizzBuzz()
     const result = fizzBuzz.generate(2)
 
     expect(result).to.equal(2)
   })
 
   it('should return "Fizz" when 3 is played', () => {
-    const fizzBuzz = new FizzBuzz()
     const result = fizzBuzz.generate(3)
 
     expect(result).to.equal("Fizz")

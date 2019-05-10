81b4da0573fe8e0fcbdc3b47786279dded8ba236
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-10 12:51:09 +0800

    feature(chapter01): Listing 1-67. Interfaces

---
+++ chapter01/src/67.ts
@@ -0,0 +1,25 @@
+// Listing 1-67. Interfaces
+
+interface Point {
+  // Properties
+  x: number
+  y: number
+}
+
+interface Passenger {
+  // Properties
+  name: string
+}
+
+interface Vehicle {
+  // Constructor
+  new (): Vehicle
+
+  // Properties
+  currentLocation: Point
+
+  // Methods
+  travelTo(point: Point): void
+  addPassenger(passenger: Passenger): void
+  removePassenger(passenger: Passenger): void
+}

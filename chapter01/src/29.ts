7efeb2bf46cbed5d4c74b228d4b7109b6832baf8
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-10 19:12:03 +0800

    refactor(chapter01): scope listing-29 with namespace

--- chapter01/src/29.ts
+++ chapter01/src/29.ts
@@ -1,35 +1,40 @@
 // Listing 1-29. Discriminated union
 
-interface Cube {
-  kind: "cube" // Discriminant
-  size: number
-}
+namespace Listing29 {
 
-interface Cuboid {
-  kind: "cuboid" // Discriminant
-  width: number
-  depth: number
-  height: number
-}
 
-// Union
-type Prism = Cube | Cuboid
-
-function volume(prism: Prism): number {
-  // Type Guard
-  switch (prism.kind) {
-    case "cube":
-      return prism.size * prism.size * prism.size
-    case "cuboid":
-      return prism.width * prism.depth * prism.height
-    default:
-      assertNever(prism)
-      break
+  interface Cube {
+    kind: "cube" // Discriminant
+    size: number
   }
 
-  return -1 // to address ts2366 warning
-}
+  interface Cuboid {
+    kind: "cuboid" // Discriminant
+    width: number
+    depth: number
+    height: number
+  }
 
-function assertNever(arg: never): never {
-  throw new Error("Possible new tagged type: " + arg)
+  // Union
+  type Prism = Cube | Cuboid
+
+  function volume(prism: Prism): number {
+    // Type Guard
+    switch (prism.kind) {
+      case "cube":
+        return prism.size * prism.size * prism.size
+      case "cuboid":
+        return prism.width * prism.depth * prism.height
+      default:
+        assertNever(prism)
+        break
+    }
+
+    return -1 // to address ts2366 warning
+  }
+
+  function assertNever(arg: never): never {
+    throw new Error("Possible new tagged type: " + arg)
+  }
+
 }

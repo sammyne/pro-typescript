24eec0e937958175794692855775683e1e626ead
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-10 19:06:53 +0800

    refactor(chapter01): scope listing-25 with namespace

--- chapter01/src/25.ts
+++ chapter01/src/25.ts
@@ -1,29 +1,33 @@
 // Listing 1-25. Type assertions
 
-interface House {
-  bedrooms: number
-  bathrooms: number
+namespace Listing25 {
+
+  interface House {
+    bedrooms: number
+    bathrooms: number
+  }
+
+  interface Mansion {
+    bedrooms: number
+    bathrooms: number
+    butlers: number
+  }
+
+  function getProperty(): House | Mansion {
+    // ...
+    return Math.random() > 0.5
+      ? { bedrooms: 1, bathrooms: 2 }
+      : { bedrooms: 3, bathrooms: 4, butlers: 5 }
+  }
+  const property = getProperty()
+
+  // OK as the property is on both House and Mansion
+  const bedroomCount = property.bedrooms
+
+  // Errors: Property 'butlers' does not exist on type 'House | Mansion'
+  //const butlerCount = property.butlers
+
+  // OK with type assertion
+  const workingButlerCount = (<Mansion>property).butlers
+
 }
-
-interface Mansion {
-  bedrooms: number
-  bathrooms: number
-  butlers: number
-}
-
-function getProperty(): House | Mansion {
-  // ...
-  return Math.random() > 0.5
-    ? { bedrooms: 1, bathrooms: 2 }
-    : { bedrooms: 3, bathrooms: 4, butlers: 5 }
-}
-const property = getProperty()
-
-// OK as the property is on both House and Mansion
-const bedroomCount = property.bedrooms
-
-// Errors: Property 'butlers' does not exist on type 'House | Mansion'
-//const butlerCount = property.butlers
-
-// OK with type assertion
-const workingButlerCount = (<Mansion>property).butlers

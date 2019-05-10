ce5084363ebe3ce61a726e4237b7dc78f324c2b4
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-10 12:30:52 +0800

    feature(chapter01): Listing 1-64. Currying with arrow functions

---
+++ chapter01/src/64.ts
@@ -0,0 +1,6 @@
+// Listing 1-64. Currying with arrow functions
+
+// Currying
+const multiply = (a: number) => (b: number) => a * b
+
+export { multiply }
--- chapter01/test/index.ts
+++ chapter01/test/index.ts
@@ -4,3 +4,30 @@
 // testing
 import "./12"
 import "./59"
+
+import { expect } from "chai"
+
+import { multiply } from "../src/64"
+
+describe("Listing 1-64. Currying with arrow functions", () => {
+  it("Pass both arguments in sequence", () => {
+    const numA = multiply(5)(6)
+    expect(numA).to.equal(30)
+  })
+
+  it("Pass just the first argument and re-use", () => {
+    const orderOfMagnitude = multiply(10)
+
+    // 10
+    const deca = orderOfMagnitude(1)
+    expect(deca).to.equal(10)
+
+    // 100
+    const hecta = orderOfMagnitude(deca)
+    expect(hecta).to.equal(100)
+
+    // 1,000
+    const kilo = orderOfMagnitude(hecta)
+    expect(kilo).to.equal(1000)
+  })
+})

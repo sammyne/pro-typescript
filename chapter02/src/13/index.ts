a03c2ee37d70238a34b3a6b94fd72f0366a898bc
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-15 10:40:47 +0800

    feature(chapter03): Listing 2-13. Importing a default

---
+++ chapter02/src/13/index.ts
@@ -0,0 +1,9 @@
+// Listing 2-13. Importing a default
+
+// Import a default export
+import Yacht from "../12"
+
+// Error: Module has no default export
+//import Ship from "../07"
+
+const yacht = new Yacht("The Sea Princess", "Tadley", 150)

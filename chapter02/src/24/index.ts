c35843bcc8f63f05d7fd6bd3c7646fad5f7461fb
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-05-15 10:53:04 +0800

    feature(chapter02): Listing 2-24. Logging Method Decorator

---
+++ chapter02/src/24/index.ts
@@ -0,0 +1,38 @@
+// Listing 2-24. Logging Method Decorator
+
+namespace Listing24 {
+  function log(target: any, key: string, descriptor: any) {
+    const original = descriptor.value
+
+    descriptor.value = function(...args: any[]) {
+      // Call the original method
+      const result = original.apply(this, args)
+
+      // Log the call, and the result
+      console.log(
+        `${key} with args ${JSON.stringify(args)} returned ${JSON.stringify(
+          result
+        )}`
+      )
+
+      return result
+    }
+    return descriptor
+  }
+
+  class Calculator {
+    // Using the decorator
+    @log
+    square(num: number) {
+      return num * num
+    }
+  }
+
+  const calculator = new Calculator()
+
+  // square with args [2] returned 4
+  calculator.square(2)
+
+  // square with args [3] returned 9
+  calculator.square(3)
+}
